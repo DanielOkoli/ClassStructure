e8819f3da88db0ef8c0e98eb0aebfbd202b47a6b
DanielOkoli <danielokoli16@gmail.com>
2023-01-22 19:28:26 +0000

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,40 @@
+/**
+ * This is as simple Java Bean Class
+ * @author Daniel Okoli
+ * @verison 11
+ */
+public class Animal {
+    //field
+    private String name;
+
+    /**
+     * get the name of animal
+     * @return String
+     */
+
+    public String getName(){
+        return name;
+    }
+
+    /**
+     *set the name of animal
+     * @param name = name of animal
+     */
+
+    public void setName (String name){
+        this.name = name;
+    }
+
+    /**
+     * run the program
+     * @param args
+     */
+
+    public static void main(String[] args){
+        System.out.println("welcome");
+    }
+}
+
+class Zoo1{
+
+}
---
+++ src/com/company/Zoo.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Zoo {
+
+    public static void main(String[] args){
+        //write your code
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
